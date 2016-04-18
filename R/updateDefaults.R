6da6c4512aa53ffee4253529643762b31806558b
Calli Gross <m.gross@mac.com>
2016-04-18 19:51:32 +0200

    Fixes a typo in updateDefaults

--- R/updateDefaults.R
+++ R/updateDefaults.R
@@ -9,14 +9,13 @@ updateDefaults <- function(gg, defaults, linetypes = linetypes) {
 
     # find elements in defaults with the class of i
     ThemeClass <- class(gg$theme[[i]])[1]
-    DefaultClasses <- sapply(default, class)
+    DefaultClasses <- sapply(defaults, class)
     DefaultClasses <- DefaultClasses[DefaultClasses == ThemeClass]
     DefaultClasses <- names(DefaultClasses)
     if (length(DefaultClasses) > 0) {
       for (j in DefaultClasses) {
         gg$theme[[i]][which(gg$theme[[i]][names(default[[j]])] == '')] <- NULL
         default[[j]][names(gg$theme[[i]])] <- lapply(gg$theme[[i]][names(gg$theme[[i]])], unname)
-
       }
     }
   }
@@ -34,7 +33,7 @@ updateDefaults <- function(gg, defaults, linetypes = linetypes) {
 
   # get common names
   NamesThemes <- names(ThemeOptions)
-  NamesDefaults <- names(unlist(default))
+  NamesDefaults <- names(unlist(defaults))
   CommonNames <- NamesThemes[NamesThemes %in% NamesDefaults]
 
   CommonNames <- CommonNames[!CommonNames %in% c('legend.position.x', 'legend.position.y')]
@@ -46,28 +45,28 @@ updateDefaults <- function(gg, defaults, linetypes = linetypes) {
     element <- gsub(pattern = '^.*\\.', '', i)
     if (i == 'legend.position' || i == 'legend.direction') {
       if (!is.null(LegendPosition) && i == 'legend.position') {
-        default['legend.position'] <- 'XY'
-        default['legend.position.x'] <- LegendPosition[1]
-        default['legend.position.y'] <- LegendPosition[2]
+        defaults['legend.position'] <- 'XY'
+        defaults['legend.position.x'] <- LegendPosition[1]
+        defaults['legend.position.y'] <- LegendPosition[2]
       } else {
-        default[i] <- gg$theme[i]
+        defaults[i] <- gg$theme[i]
       }
     }
     else {
       # Relative size needs to be converted to absolute sizes
       if (class(gg[['theme']][[anchor]][[element]]) == 'rel') {
-        default[[anchor]][[element]] <- as.numeric(gg[['theme']][[anchor]][[element]]) * gg$theme$text$size
+        defaults[[anchor]][[element]] <- as.numeric(gg[['theme']][[anchor]][[element]]) * gg$theme$text$size
       } else {
         if (element == 'linetype' && is.numeric(gg[['theme']][[anchor]][[element]])) {
-          default[[anchor]][[element]] <- linetypes[gg[['theme']][[anchor]][[element]] + 1]
+          defaults[[anchor]][[element]] <- linetypes[gg[['theme']][[anchor]][[element]] + 1]
         } else {
-          default[[anchor]][[element]] <- gg[['theme']][[anchor]][[element]]
+          defaults[[anchor]][[element]] <- gg[['theme']][[anchor]][[element]]
         }
       }
     }
   }
 
   # substitute grey with gray
-  default <- rapply(default, function(x) { gsub('grey', 'gray', x) }, how = 'list')
-  return(default)
+  defaults <- rapply(defaults, function(x) { gsub('grey', 'gray', x) }, how = 'list')
+  return(defaults)
 }
